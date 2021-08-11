a5331c0bb03c574687476affdbcd9d3265a379e2
Atul Jha <69789941+Atul240202@users.noreply.github.com>
2021-08-12 01:13:47 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,13 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAwMBc-sofrFQnyHSHy_UY00ARMWZCuwiY",
+    authDomain: "login-page-6f93b.firebaseapp.com",
+    projectId: "login-page-6f93b",
+    storageBucket: "login-page-6f93b.appspot.com",
+    messagingSenderId: "1051272926680",
+    appId: "1:1051272926680:web:c9af8f5186be9dcf7ecead",
+    measurementId: "G-P6N8B2CTSW"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
